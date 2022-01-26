c1942349ee3f097b6aaa343734f1e596c09cd136
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-01-26 11:50:35 +0100

    class done

--- exercises/01-exe-js_async_callbacks/scripts/script.js
+++ exercises/01-exe-js_async_callbacks/scripts/script.js
@@ -6,13 +6,43 @@ function print() {
     setTimeout(() => console.log(3), 0);
     console.log(4);
   }
-  
-  print(); 
+  //1 4 3 2 -> because of the TimeOut in "setTimeOut" function
+ // print(); 
   
   // ------------------------------------------------------------------------
   
   
-  //Exercise 2: Write a function blast() , which loops over the numbers 1 to 15. every 1 second 
+  //Exercise 2: Write a function blast() , which loops over the numbers 1 to 15. every 1 second
+
+  const blast = () => {
+
+    let i = 1;
+    const number = document.querySelector(".number");
+    const words = document.querySelector(".words");
+
+    const myInterval=setInterval(() => {
+      number.textContent = i;
+      if(i%15===0) {
+        console.log("BOOM BANG!!");
+        words.innerText = "BOOM BANG!!";
+      }
+      else if(i%3===0) {
+        console.log("BOOM!!");
+        words.innerText = "BOOM!!";
+      }
+      else if(i%5===0) {
+        console.log("BANG!!");
+        words.innerText = "BANG!!";
+      }
+      else
+      words.innerText = "";
+
+      if(i===15) clearInterval(myInterval);
+      i++;
+    }, 1000);
+  };
+
+  blast();
   // - For every number divisible by 3 it prints BOOM!!
   // - For every number divisible by 5 it prints a BANG!!
   // - Forevery number divisible for 3 and 5 it prints BOOM BANG!! 
